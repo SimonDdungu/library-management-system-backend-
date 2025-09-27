164270d3ccad8c920edb4ed14acb6a1779abc791
SimonDdungu <dev.simonddungu@gmail.com>
2025-09-27 21:42:03 +0300

    Updated Book Controller

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,11 +1,14 @@
 import { Request } from "express"
 import { services } from '../services'
+import { searchFilters } from "../interfaces";
 import _ from "lodash";
 
 const Joi = require('joi');
 
+
+
 class BookController {
-    async getAllBooks(filters: {page?: number, sortedBy?: string, order?: "asc" | "desc"}){
+    async getAllBooks(filters: searchFilters){
         try {
             const {page = 1, sortedBy = "createdAt", order = "desc"} = filters || {}
 
@@ -20,7 +23,7 @@ class BookController {
             const schema = Joi.object({
                 title: Joi.string().required(),
                 author: Joi.string().required(),
-                publish_year: Joi.number().integer().required(),
+                publish_year: Joi.number().integer().required().max(4),
                 isbn: Joi.string().required()
             })
 
@@ -44,7 +47,7 @@ class BookController {
         }
     }
 
-    async findBooks(req: Request, filters: {page?: number, sortedBy?: string, order?: "asc" | "desc"}){
+    async findBooks(req: Request, filters: searchFilters){
         try {
             const schema = Joi.object({
             query: Joi.string().required()
@@ -60,10 +63,127 @@ class BookController {
         } catch (error) {
             throw error
         }
-        
+    }
+
+    async findBookTitle(req: Request, filters: searchFilters){
+        try {
+            const schema = Joi.object({
+            title: Joi.string().required()
+            })
+
+            const payload = await schema.validateAsync(req.body)
+
+            const {title} = payload
+            const {page = 1, sortedBy = "title", order = "asc"} = filters || {}
+
+            return await services.books.findBookTitle(title, {page, sortedBy, order})
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async findBookAuthor(req: Request, filters: searchFilters){
+        try {
+            const schema = Joi.object({
+            author: Joi.string().required()
+            })
+
+            const payload = await schema.validateAsync(req.body)
+
+            const {author} = payload
+            const {page = 1, sortedBy = "author", order = "asc"} = filters || {}
+
+            return await services.books.findBookAuthor(author, {page, sortedBy, order})
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async findByYear(req: Request, filters: searchFilters){
+        try {
+            const schema = Joi.object({
+            year: Joi.number().integer().required().max(4)
+            })
+
+            const payload = await schema.validateAsync(req.body)
+
+            const {year} = payload
+            const {page = 1, sortedBy = "author", order = "asc"} = filters || {}
+
+            return await services.books.findByYear(year, {page, sortedBy, order})
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async findByISBN(req: Request){
+        try {
+            const schema = Joi.object({
+            isbn: Joi.string().required()
+            })
+
+            const payload = await schema.validateAsync(req.body)
+
+            const {isbn} = payload
+    
+            return await services.books.findByISBN(isbn)
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    
+    async updateOneBook(req: Request){
+        try {
+            const schema = Joi.object({
+                id: Joi.string().required(),
+                title: Joi.string().required(),
+                author: Joi.string().required(),
+                publish_year: Joi.number().integer().required().max(4),
+                isbn_id: Joi.string().required(),
+                new_isbn: Joi.string().required(),
+            })
+
+            const payload = await schema.validateAsync(req.body)
+
+            if(payload.error == null){
+                const {id, title, author, publish_year, isbn_id, new_isbn} = payload
+                const Title = _.startCase(title)
+                const Author = _.startCase(author)
+
+                const exists = await services.books.findByISBN(new_isbn)
+
+                if(exists){
+                    throw new Error("Book with ISBN already exists")
+                }else{
+                    return await services.books.updateOneBook(id, Title, Author, publish_year, isbn_id, new_isbn)
+                }
+            } 
+        } catch (error) {
+            throw error
+        }
+    }
 
 
+    async deleteOneBook(req: Request){
+        try {
+            const schema = Joi.object({
+            id: Joi.string().required()
+            })
 
+            const payload = await schema.validateAsync(req.body)
+
+            const {id} = payload
+    
+            return await services.books.deleteOneBook(id)
+
+        } catch (error) {
+            throw error
+        }
     }
 }
 
