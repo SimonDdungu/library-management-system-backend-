a2f868d9a1a559614fcae11f1dc478a715cdf4a6
SimonDdungu <dev.simonddungu@gmail.com>
2025-09-30 22:46:30 +0300

    Updated Controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,7 @@
 import { BookController } from "./bookController";
+import { UserController } from "./userController";
 
 export const controllers = {
     books: new BookController(),
+    users: new UserController(),
 }
