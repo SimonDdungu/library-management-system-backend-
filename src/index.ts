a45e7498f89b2d53de36197309d1b48b468f9f89
SimonDdungu <dev.simonddungu@gmail.com>
2025-09-30 22:25:59 +0300

    Minor update

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 3008
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use("/api/v1/books/", bookRoutes)
+app.use("/api/v1/books", bookRoutes)
 
 app.listen(PORT, () => console.log(`Libary Management system running on localhost:${PORT}`))
