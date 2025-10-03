de4e23c0c108174e4bee832947f2cce0f58d02cd
SimonDdungu <dev.simonddungu@gmail.com>
2025-10-03 19:01:37 +0300

    Added Error Handler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import errorHandler from "./middleware/errorHandler"
 const express = require("express")
 const bodyParser = require("body-parser")
 const bookRoutes = require("./routes/bookRoutes")
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3008
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(errorHandler)
 app.use("/api/v1/books", bookRoutes)
 app.use("/api/v1/users", userRoutes)
 
