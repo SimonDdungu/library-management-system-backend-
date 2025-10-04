ca55eb05095bd8ab4415d94853c4707d9f2482b5
SimonDdungu <dev.simonddungu@gmail.com>
2025-10-04 17:54:24 +0300

    Added Access Control

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,11 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { config } from "../config/env";
+import { services } from "../services";
+
+interface UserAccess {
+    id: string,
+}
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     let token;
@@ -24,4 +29,54 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) => 
     } else {
         return res.status(401).json({status: 401, message: "No Token, Authorization denied"})
     }
+}
+
+
+
+export const isStaff = async(user: UserAccess) => {
+    const validUser = await services.admin.findById(user.id)
+
+    if (!validUser){
+        throw new Error("User not found")
+    } 
+
+    return (
+        (validUser.position.roleId >= 1 && validUser.position.roleId <= 4)
+    )
+}
+
+export const isLibrarian = async(user: UserAccess) => {
+   const validUser = await services.admin.findById(user.id)
+
+    if (!validUser){
+        throw new Error("User not found")
+    } 
+
+    return (
+        (validUser.position.roleId >= 1 && validUser.position.roleId <= 3)
+    )
+}
+
+export const isAdmin = async(user: UserAccess) => {
+    const validUser = await services.admin.findById(user.id)
+
+    if (!validUser){
+        throw new Error("User not found")
+    } 
+
+    return (
+        (validUser.position.roleId >= 1 && validUser.position.roleId <= 2)
+    )
+}
+
+export const isSuperAdmin = async(user: UserAccess) => {
+    const validUser = await services.admin.findById(user.id)
+
+    if (!validUser){
+        throw new Error("User not found")
+    } 
+
+    return (
+        validUser.position.roleId === 1
+    )
 }
