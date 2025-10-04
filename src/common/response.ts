74c39491a9c0c8177a4468b06c1c562f42aa2484
SimonDdungu <dev.simonddungu@gmail.com>
2025-10-04 18:35:50 +0300

    Updated Handle response

--- src/common/response.ts
+++ src/common/response.ts
@@ -4,22 +4,24 @@ import { Response } from 'express'
 const sendSuccessMessage = (res: Response, data?: any) => {
     const response = {
         statusCode: 200,
+        message: "Authorized, Successfully retrieved data.",
         data: data || {}
     };
     res.status(response.statusCode).send(response);
 }
 
-const sendErrorMessage = (res: Response, error: any) => {
-    let message = error.message;
+const handleErrorResponse = (res: Response, status: number, error: any, message?: string) => {
+    let errorMessage = error.message;
     if (error.isJoi) {
-        message = error.details[0].message;
+        errorMessage = error.details[0].message ;
     }
 
     const response = {
-        statusCode: 400,
-        message: message
+        statusCode: status || 500,
+        message: message || "Internal Server Error",
+        error: errorMessage,
     }
     res.status(response.statusCode).send(response);
 }
 
-export { sendSuccessMessage, sendErrorMessage }
+export { sendSuccessMessage, handleErrorResponse }
